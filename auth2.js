e944548d9f1c4a07725796f44076c1e14a9587c7
corinahincu <117738625+corinahincu@users.noreply.github.com>
2023-09-16 14:04:18 +0200

    Add files via upload

--- auth2.js
+++ auth2.js
@@ -1,24 +1,21 @@
-// auth.js
-const { v4: uuidv4 } = require("uuid");
 
+const { v4: uuidv4 } = require("uuid");
+//signin
 async function signIn(email, password, fastify) {
   const pool = fastify.pgPool;
   try {
-    // Check if a client with the given email and password exists
     const { rows } = await pool.query(
       "SELECT id FROM clients WHERE email = $1 AND password = $2",
       [email, password]
     );
 
     if (rows.length === 0) {
-      return null; // User not found
+      return null; 
     }
 
-    // Generate a session_id using a UUID library 
     const session_id = uuidv4();
-    const client_id = rows[0].id // to get the client's id
+    const client_id = rows[0].id 
 
-    // Insert a new session record in client_sessions
     await pool.query(
       "INSERT INTO client_sessions (session_id, client_id) VALUES ($1, $2)",
       [session_id, client_id]
@@ -29,11 +26,10 @@ async function signIn(email, password, fastify) {
     throw error;
   }
 }
-
+//signout
 async function signOut(session_id, fastify) {
   const pool = fastify.pgPool;
   try {
-    // Delete the session record based on session_id
     await pool.query("DELETE FROM client_sessions WHERE session_id = $1", [
       session_id
     ]);
