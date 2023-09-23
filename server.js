b94856555c9a893cd382a12f0c914e826912d50e
corinahincu <117738625+corinahincu@users.noreply.github.com>
2023-09-24 00:27:55 +0200

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const {signIn,signOut}= require("./auth")
     user: "postgres",
     host: "127.0.0.1",
     database: "js_shop",
-    password: "8929c",
+    password: "1234c",
     port: 5432,
   });
 fastify.decorate("pgPool",pool)
