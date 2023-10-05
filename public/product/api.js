ea55feb905e8eb626f3a7ef2ec018fba6620868e
corinahincu <117738625+corinahincu@users.noreply.github.com>
2023-10-05 10:37:44 +0200

    Add files via upload

--- public/product/api.js
+++ public/product/api.js
@@ -6,16 +6,17 @@ const entities_js_2 = require("../financial/entities.js");
 const ProductRoute = async (fastify) => {
     fastify.post('/products', async (request, reply) => {
         try {
+            // Extract data from the request body or wherever you are getting it from
+            let requestBody = request.body;
             const product = new entities_js_1.Product();
             const money = new entities_js_2.Money();
-            product.id = 1;
-            product.name = "Macbook";
-            money.amount = 1400;
-            money.currency = "EUR";
+            product.name = requestBody.name;
+            money.amount = requestBody.amount;
+            money.currency = requestBody.currency;
             product.price = money;
             const productRepository = (0, typeorm_1.getRepository)(entities_js_1.Product);
             await productRepository.save(product);
-            reply.status(201).send({ message: 'Product created successfully' });
+            reply.status(200).send({ message: 'Product created successfully' });
         }
         catch (error) {
             console.error('Error creating product:', error);
@@ -24,3 +25,28 @@ const ProductRoute = async (fastify) => {
     });
 };
 exports.default = ProductRoute;
+/* const ProductRoute = async (fastify: FastifyInstance) => {
+  fastify.post('/products', async(request,reply) =>{
+    try{
+      const requestBody: CreateProductRequest = request.body;
+
+      const product = new Product()
+      const money = new Money()
+      product.id = requestBody.id;
+      product.name = requestBody.name;
+      money.amount = requestBody.amount;
+      money.currency = requestBody.currency;
+
+      product.price = money
+
+      const productRepository = getRepository(Product)
+      await productRepository.save(product)
+
+      reply.status(201).send({ message: 'Product created successfully'})
+    } catch(error){
+      console.error('Error creating product:', error)
+      reply.status(500).send({message: 'Internal server error'})
+    }
+  })
+}
+export default ProductRoute */ 
--- src/product/api.ts
+++ src/product/api.ts
@@ -1,18 +1,55 @@
 import {FastifyInstance} from 'fastify'
+import { fastify } from 'fastify';
 import { getRepository } from 'typeorm'
 import {Product} from './entities.js'
 import { Money } from '../financial/entities.js'
 
+interface ProductRequest {
+  id: number;
+  name: string;
+  amount: number;
+  currency: string;
+}
+
 const ProductRoute = async (fastify: FastifyInstance) => {
+  fastify.post('/products', async (request, reply) => {
+    try {
+      // Extract data from the request body or wherever you are getting it from
+      let requestBody: ProductRequest = request.body as ProductRequest
+
+      const product = new Product();
+      const money = new Money();
+      product.name = requestBody.name;
+      money.amount = requestBody.amount;
+      money.currency = requestBody.currency;
+      product.price = money;
+
+      const productRepository = getRepository(Product);
+      await productRepository.save(product);
+
+      reply.status(200).send({ message: 'Product created successfully' });
+    } catch (error) {
+      console.error('Error creating product:', error);
+      reply.status(500).send({ message: 'Internal server error' });
+    }
+  });
+};
+    export default ProductRoute
+
+
+
+/* const ProductRoute = async (fastify: FastifyInstance) => {
   fastify.post('/products', async(request,reply) =>{
     try{
+      const requestBody: CreateProductRequest = request.body;
 
       const product = new Product()
       const money = new Money()
-      product.id = 1
-      product.name = "Macbook"
-      money.amount = 1400
-      money.currency = "EUR"
+      product.id = requestBody.id;
+      product.name = requestBody.name;
+      money.amount = requestBody.amount;
+      money.currency = requestBody.currency;
+
       product.price = money
 
       const productRepository = getRepository(Product)
@@ -25,4 +62,4 @@ const ProductRoute = async (fastify: FastifyInstance) => {
     }
   })
 }
-export default ProductRoute
+export default ProductRoute */
